7cb90f12bbdb2a7d5b195cf4831cce3e7c7b2a7b
DiegoPla1104 <diegopla1104@gmail.com>
2024-01-03 21:30:10 +0000

    feat: task 3

---
+++ python_async_function/3-tasks.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Module"""
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int):
+    """Function that takes an integer"""
+    return asyncio.create_task(wait_random(max_delay))
