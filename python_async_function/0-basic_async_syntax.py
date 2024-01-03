8d80f4b0f8023045aa947cb8ac82b30650ae20b9
DiegoPla1104 <diegopla1104@gmail.com>
2024-01-03 18:23:56 +0000

    fix?

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """Module"""
-from random import uniform
-from asyncio import sleep
+import random
+import asyncio
 
 
 async def wait_random(max_delay: int = 10) -> float:
     """Asynchronous coroutine"""
-    delay = uniform(0, max_delay)
-    await sleep(delay)
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
     return delay
