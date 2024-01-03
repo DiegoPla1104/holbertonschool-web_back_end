c819bd45539eedebc41c9c89a9bce69c737bffe6
DiegoPla1104 <diegopla1104@gmail.com>
2024-01-03 20:51:27 +0000

    feat: task 2

---
+++ python_async_function/2-measure_runtime.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Module"""
+import time
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+async def measure_time(n: int, max_delay: int) -> float:
+    """Measures the elapsed time"""
+    start_time = time.time()
+    await wait_n(n, max_delay)
+    end_time = time.time()
+    return (start_time - end_time)
