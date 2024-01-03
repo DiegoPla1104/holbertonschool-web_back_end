5dc94408f376872665b9057be70f878795c5063d
DiegoPla1104 <diegopla1104@gmail.com>
2024-01-03 01:32:40 +0000

    feat: task 6

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+from typing import List, Union
+"""Module"""
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """Sums all members of the list"""
+    return float(sum(mxd_lst))
