4a4297b6476e984c8451967dc203d5724bf1ae44
DiegoPla1104 <diegopla1104@gmail.com>
2024-01-03 01:23:38 +0000

    feat: task 5

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+from typing import List
+"""Module"""
+
+
+def sum_list(input_list: List[float]) -> float:
+    """Sums all members of the list"""
+    return sum(input_list)
