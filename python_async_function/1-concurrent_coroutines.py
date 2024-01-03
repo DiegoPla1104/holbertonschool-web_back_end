05d6febc751ada7d85a46ccbca7aa7dd453ef009
DiegoPla1104 <diegopla1104@gmail.com>
2024-01-03 20:12:27 +0000

    feat: task 1

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Module"""
+wait_random = __import__('0-basic_async_syntax').wait_random 
+
+
+async def wait_n(n: int, max_delay: int):
+    """Function to wait n number of times"""
+    i = 0
+    dic = []
+    for i in range(0, n):
+        result = await wait_random(max_delay)
+        dic.append(result)
+        i += 1
+    return sorted(dic)
+    
