f3febb0b8eb285f2c55f0901a2b220389623f19e
DiegoPla1104 <diegopla1104@gmail.com>
2024-01-03 20:25:40 +0000

    fix: forgotten imports

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 """Module"""
+import asyncio
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int):
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """Function to wait n number of times"""
     i = 0
     dic = []
@@ -11,4 +13,4 @@ async def wait_n(n: int, max_delay: int):
         result = await wait_random(max_delay)
         dic.append(result)
         i += 1
-    return dic
+    return sorted(dic)
