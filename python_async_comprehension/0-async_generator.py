dd2d20b9fdc910d05ed41f5e45e5efa179b92e5b
DiegoPla1104 <diegopla1104@gmail.com>
2024-01-04 17:38:01 +0000

    fix: missing commentary in function

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -5,6 +5,7 @@ import asyncio
 
 
 async def async_generator() -> float:
+    """Function o create ten floats"""
     for i in range(0, 10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
