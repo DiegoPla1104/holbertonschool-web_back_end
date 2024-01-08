16d8d2d74124d3e44156367118720bf1e4a080eb
DiegoPla1104 <diegopla1104@gmail.com>
2024-01-08 21:14:02 +0000

    fix?

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -6,4 +6,4 @@ import pymongo
 def update_topics(mongo_collection, name, topics):
     """Changes all topics of a school document based on the name """
     return mongo_collection.update_many({"name": name}, {"$\
-        set": {"topics": topics}})
+set": {"topics": topics}})
