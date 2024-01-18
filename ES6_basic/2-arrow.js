1b9b623603fa65489a4ff2c3bde5a8faa0dfeed4
DiegoPla1104 <diegopla1104@gmail.com>
2024-01-18 13:32:53 +0000

    feat: task 2

--- ES6_basic/2-arrow.js
+++ ES6_basic/2-arrow.js
@@ -2,7 +2,8 @@ export default function getNeighborhoodsList() {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
   const self = this;
-  this.addNeighborhood = function add(newNeighborhood) {
-    return self.sanFranciscoNeighborhoods.push(newNeighborhood);
+  this.addNeighborhood = function (newNeighborhood) => {
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
   };
 }
